02c594a119fc85d9545b9543124fd2c417b73d10
정지창 <junggi0729@naver.com>
2023-09-06 10:14:51 +0900

    회원정보 수정 merge

--- src/main/java/com/example/feelog/Controller/MemberController.java
+++ src/main/java/com/example/feelog/Controller/MemberController.java
@@ -6,10 +6,7 @@ import com.example.feelog.Entity.Member;
 import com.example.feelog.Service.RegisterService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
@@ -59,6 +56,59 @@ public class MemberController {
         mv.setViewName("index.html");
 
         return mv;
+    }
+
+    @RequestMapping ("/memberUpdate/{memberId}")
+    public ModelAndView update(@PathVariable Long memberId) {
+        Optional<Member> optionalMember = registerService.getById(memberId);
+
+        if (optionalMember.isPresent()) {
+            Member member = optionalMember.get();
+            ModelAndView mv = new ModelAndView("updateForm.html");
+            mv.addObject("member", member);
+            return mv;
+        } else {
+            // 해당 회원을 찾을 수 없는 경우에 대한 처리
+            // 에러 메시지를 포함한 ModelAndView를 반환합니다.
+            ModelAndView mv = new ModelAndView("updateForm");
+            mv.addObject("member", new Member()); // 빈 Member 객체를 생성하여 폼을 초기화
+            mv.addObject("errorMessage", "해당 회원을 찾을 수 없습니다."); // 에러 메시지를 추가
+            return mv;
+        }
+    }
+
+    @PostMapping("/memberUpdateAction/{memberId}")
+    public ModelAndView updateAction(@PathVariable Long memberId,
+                                     @RequestParam String newName,
+                                     @RequestParam String newEmail,
+                                     @RequestParam String newPassword,
+                                     @RequestParam String newIntroduce,
+                                     HttpSession session) {
+        session.setAttribute("login",registerService.updateMember(memberId, newName, newEmail, newPassword, newIntroduce));
+        return new ModelAndView("redirect:/index");
+    }
+
+    @GetMapping("/deleteMember/{memberId}")
+    public ModelAndView deleteMember(@PathVariable Long memberId, HttpSession session) {
+        Optional<Member> optionalMember = registerService.getById(memberId);
+        System.out.println("memberId"+memberId);
+        if (optionalMember.isPresent()) {
+            // 해당 회원을 찾은 경우에 삭제 처리를 진행합니다.
+            registerService.deleteMember(memberId);
+
+            // 세션에서 로그인 정보를 제거합니다.
+            session.setAttribute("login",null);
+
+            // 회원 삭제 후 어떤 페이지로 리다이렉트할지 지정합니다.
+            return new ModelAndView("index.html"); // 홈 페이지로 리다이렉트 또는 다른 페이지로 설정 가능
+        } else {
+            // 해당 회원을 찾을 수 없는 경우에 대한 처리
+            // 에러 메시지를 포함한 ModelAndView를 반환합니다.
+            ModelAndView mv = new ModelAndView("updateForm");
+            mv.addObject("member", new Member()); // 빈 Member 객체를 생성하여 폼을 초기화
+            mv.addObject("errorMessage", "해당 회원을 찾을 수 없습니다."); // 에러 메시지를 추가
+            return mv;
+        }
     }
 
 
